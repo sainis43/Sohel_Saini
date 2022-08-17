d5ce85adb7c54bbe8d46a4b0c3f4669809e8d010
Sohel Saini <sainis43@mcmaster.ca>
2022-08-16 22:34:52 -0400

    Animation addition

--- scriptforcolour.js
+++ scriptforcolour.js
@@ -1,5 +1,6 @@
 console.log('Its working')
 
+//Changing theme colour + local save
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
@@ -39,3 +40,44 @@ function setTheme(mode){
 	localStorage.setItem('theme', mode)
 
 } 
+
+
+// Mobile Menu -> Hamburger
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
+const subtitle = document.querySelector("#type1");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+  subtitle.classList.toggle("hide")
+});
+
+// AOS
+AOS.init();
+
+// Typing Animation
+new TypeIt("type1", {
+  speed: 90,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("a Computer Engineer.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Web Developer.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("an Athlete.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Software Developer.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Problem Solver.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("an Analytical Thinker.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .go();
